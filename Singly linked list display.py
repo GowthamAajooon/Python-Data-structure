6c59db74f3c3cadc95b29f387392a19049b437e2
Gowtham <79574853+GowthamAajooon@users.noreply.github.com>
2023-11-28 10:47:23 +0530

    Update Singly linked list display.py

--- Singly linked list display.py
+++ Singly linked list display.py
@@ -1,1 +1,15 @@
+class node:
+  def __init__(self,data):
+    self.data = data
+    self.next = None
+def display():
+  pass
 
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+
+node1 = node2.next
+node2 = node3.next
+
+display(node1)
